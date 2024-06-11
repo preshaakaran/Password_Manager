7eaec6b31e2c21336396a0f40ad22377d818266b
Presha Karan <159997342+preshaakaran@users.noreply.github.com>
2024-06-12 02:23:07 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
-import { useState } from 'react'
-import Navbar from './Components/Navbar'
-import Manager from './Components/Manager'
-import Footer from './Components/Footer'
-import './App.css'
+import { useState } from 'react';
+import Navbar from './Components/Navbar';
+import Manager from './Components/Manager';
+import Footer from './Components/Footer';
+import './App.css';
 
 function App() {
-  
-
   return (
     <>
-      <Navbar/>
-      <div className='min-h-[84.9vh] '>
-      <div className="p-5 absolute top-0 z-[-2] h-screen w-screen bg-green-50 bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]">
-      <Manager/>
+      <Navbar />
+      <div className="p-5 top-0 z-[-2] h-screen w-screen bg-green-50 bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]" style={{ height: '85vh' }}>
+        
+          <Manager />
+        
       </div>
-      
-      </div>
-      <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
